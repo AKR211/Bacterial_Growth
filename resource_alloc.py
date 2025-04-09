b47468aae775cd51597662b8ad5b1a173574b797
AKR211 <agneyk.rajeev@niser.ac.in>
2025-04-09 17:10:48 +0530

    Init commit for resource allocation plot

---
+++ resource_alloc.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from Utils import simulate
+
+def main(N):
+
+    params = {
+    "zeta": 1e-3,
+    "n_0": 1.0,
+    "eps": 1e-6,
+    "omega": 0.5,
+    "phi": 0.5,
+    "kQ": 1e-9,
+    "kT": 10.0,
+    "s0": 10
+    }
+
+    phis = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8] # Different values of phi to test
+    omegas = [0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0] # Different values of omega to test
+
+    doubling_times_list = []
+    cell_types_list = []
+    n_list = []
+    r_list = []
+    s_list = []
+    m_list = []
+
+    PHIs, OMEGAs = np.meshgrid(phis, omegas) # Create a meshgrid for phi and omega
+    PHIs = PHIs.flatten()
+    OMEGAs = OMEGAs.flatten()
+
+    for idx in tqdm(range(len(PHIs)), desc="phi-omega loop", total=len(PHIs)):
+
+        params["phi"] = PHIs[idx]
+        params["omega"] = OMEGAs[idx]
+
+        # Simulate growth of N cells with the given parameters
+        cells, cell_types = simulate(params, N, tqdm_flag=False) # 'cells' has shape (n_cells, n_steps, n_variables) and 'cell_types' has shape (n_cells,)
+        doubling_times = []
+        cell_contents = []
+        for cell in cells:
+            c = np.array(cell).T # growth data of the cell
+            val = np.average(c[:4], weights=c[-1], axis=1) # Calculate the average values of the n, r, s, m weighted with time
+            t = np.sum(c[-1]) # Total doubling time of the cell
+            doubling_times.append(t)
+            cell_contents.append(val)
+
+        n, r, s, m = np.array(cell_contents).T
+
+        doubling_times, cell_types = np.array(doubling_times), np.array(cell_types)
+
+        doubling_times_list.append(doubling_times)
+        cell_types_list.append(cell_types)
+        n_list.append(n)
+        r_list.append(r)
+        s_list.append(s)
+        m_list.append(m)
+
+    return phis, omegas, doubling_times_list, cell_types_list, n_list, r_list, s_list, m_list
+
+if __name__ == "__main__":
+    import netCDF4 as nc
+    import os
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Simulate cell growth and save results to a NetCDF file.")
+    parser.add_argument("--n", type=int, default=1e4, help="Number of cells to simulate.")
+    parser.add_argument("--rpt", type=int, default=10, help="Number of repetitions for the simulation.")
+
+    args = parser.parse_args()
+    N = args.n
+    repeats = args.rpt
+
+    if not os.path.exists("./data/"):
+        os.makedirs("./data/")
+
+    #phi, omegas, doubling_times_list, cell_types_list, n_list, r_list, s_list, m_list = main()
+
+    tot = []
+    for rpt in tqdm(range(repeats), desc="Repeating"):
+        out = main(int(N))
+        tot.append(out[2:])
+        if rpt == 0:
+            phi, omegas = out[0], out[1]
+
+    doubling_times_list, cell_types_list, n_list, r_list, s_list, m_list = np.concatenate(tot, axis=-1)
+
+    ncfile = nc.Dataset("./data/resource_alloc.nc", "w", format="NETCDF4")
+    ncfile.createDimension("phi-omega", len(phi)*len(omegas))
+    ncfile.createDimension("phi", len(phi))
+    ncfile.createDimension("omega", len(omegas))
+    ncfile.createDimension("n_cells", len(doubling_times_list[0]))
+    ncfile.createVariable("phi", "f8", ("phi",))[:] = phi
+    ncfile.createVariable("omega", "f8", ("omega",))[:] = omegas
+    ncfile.createVariable("doubling_times", "f8", ("phi-omega", "n_cells"))[:,:] = np.array(doubling_times_list)
+    ncfile.createVariable("cell_types", "i4", ("phi-omega", "n_cells"))[:,:] = np.array(cell_types_list)
+    ncfile.createVariable("n", "f8", ("phi-omega", "n_cells"))[:,:] = np.array(n_list)
+    ncfile.createVariable("r", "f8", ("phi-omega", "n_cells"))[:,:] = np.array(r_list)
+    ncfile.createVariable("s", "f8", ("phi-omega", "n_cells"))[:,:] = np.array(s_list)
+    ncfile.createVariable("m", "f8", ("phi-omega", "n_cells"))[:,:] = np.array(m_list)
+    ncfile.close()
+
+    print("\nData saved successfully to resource_alloc.nc")
---
+++ resource_alloc_plot.py
@@ -0,0 +1,113 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.ma as ma
+
+def plot1(phis, omegas, n_list, r_list, m_list, doubling_times_list):
+
+    m = np.array(m_list).mean(axis=1).reshape(len(omegas), len(phis)).T
+    n = np.array(n_list).mean(axis=1).reshape(len(omegas), len(phis)).T
+    r = np.array(r_list).mean(axis=1).reshape(len(omegas), len(phis)).T
+    gr = np.array(np.log(2)/doubling_times_list).mean(axis=1).reshape(len(omegas), len(phis)).T
+
+    plt.figure(figsize=(10, 8))
+
+    plt.subplot(2, 2, 1)
+    plt.imshow(m, aspect='auto', cmap='viridis')
+    plt.colorbar()
+
+    plt.xticks(ticks=np.arange(len(omegas)), labels=omegas, rotation=45)
+    plt.yticks(ticks=np.arange(len(phis)), labels=phis)
+    plt.xlabel(r'$\phi$', fontsize=14)
+    plt.ylabel(r'$\omega$', fontsize=14)
+    plt.title('m', fontsize=20)
+
+    plt.subplot(2, 2, 2)
+
+    plt.imshow(r, aspect='auto', cmap='viridis')
+    plt.colorbar()
+
+    plt.xticks(ticks=np.arange(len(omegas)), labels=omegas, rotation=45)
+    plt.yticks(ticks=np.arange(len(phis)), labels=phis)
+    plt.xlabel(r'$\phi$', fontsize=14)
+    plt.ylabel(r'$\omega$', fontsize=14)
+    plt.title('r', fontsize=20)
+
+    plt.subplot(2, 2, 3)
+
+    plt.imshow(n, aspect='auto', cmap='viridis')
+    plt.colorbar()
+
+    plt.xticks(ticks=np.arange(len(omegas)), labels=omegas, rotation=45)
+    plt.yticks(ticks=np.arange(len(phis)), labels=phis)
+    plt.xlabel(r'$\phi$', fontsize=14)
+    plt.ylabel(r'$\omega$', fontsize=14)
+    plt.title('n', fontsize=20)
+
+    plt.subplot(2, 2, 4)
+
+    plt.imshow(gr, aspect='auto', cmap='inferno')
+    plt.colorbar()
+
+    plt.xticks(ticks=np.arange(len(omegas)), labels=omegas, rotation=45)
+    plt.yticks(ticks=np.arange(len(phis)), labels=phis)
+    plt.xlabel(r'$\phi$', fontsize=14)
+    plt.ylabel(r'$\omega$', fontsize=14)
+    plt.title('Growth Rate', fontsize=20)
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.savefig("./figures/resource_alloc.png", dpi=300)
+    plt.close()
+
+def plot2(phis, omegas, cell_types):
+    
+    cell_types = np.transpose(np.array(cell_types).reshape(len(omegas), len(phis), cell_types.shape[1]), (1, 0, 2))
+
+    plt.figure(figsize=(15, 5))
+
+    for i in range(1,4):
+
+        cells = ma.array(cell_types)
+        total = cells.count(axis=2)
+        cells.mask = cells != i
+        count = cells.count(axis=2)
+
+        fraction = count / total
+        plt.subplot(1, 3, i)
+
+        plt.imshow(fraction, aspect='auto', cmap='winter', norm='log', vmin = 1e-9, vmax=1)
+        plt.colorbar()
+
+        plt.xticks(ticks=np.arange(len(omegas)), labels=omegas, rotation=45)
+        plt.yticks(ticks=np.arange(len(phis)), labels=phis)
+        plt.xlabel(r'$\phi$', fontsize=14)
+        plt.ylabel(r'$\omega$', fontsize=14)
+
+        plt.title(f'Fraction of cell type {i}', fontsize=20)
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.show()
+
+        
+
+
+if __name__ == "__main__":
+    import os
+    import netCDF4 as nc
+
+    if not os.path.exists("./figures/"):    
+        os.makedirs("./figures/")
+
+    ncfile = nc.Dataset("./data/resource_alloc.nc", "r")
+    n_list = ncfile.variables["n"][:,:]
+    r_list = ncfile.variables["r"][:,:]
+    m_list = ncfile.variables["m"][:,:]
+    phis = ncfile.variables["phi"][:]
+    omegas = ncfile.variables["omega"][:]
+    doubling_times_list = ncfile.variables["doubling_times"][:,:]
+    cell_types = ncfile.variables["cell_types"][:,:]
+    ncfile.close()
+
+    plot1(phis, omegas, n_list, r_list, m_list, doubling_times_list)
+    plot2(phis, omegas, cell_types)
